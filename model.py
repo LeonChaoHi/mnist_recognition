445ee49218dfe888353e006d0f4359c89ebfdd39
LeonChaoHi <15201161716@163.comi>
2021-01-12 00:32:02 +0800

    modified network size

--- model.py
+++ model.py
@@ -5,14 +5,17 @@ from keras import optimizers
 from keras.layers import Conv2D, MaxPool2D, MaxPooling2D, Dense, Flatten, Dropout, Lambda, Reshape, GlobalAveragePooling2D
 
 
-def full_connect():
+def full_connect(lr=0.01):
     model = Sequential()
-    model.add(Dense(512, input_shape=(784,), activation='relu', name='layer1'))
-    model.add(Dense(512, activation='relu', name='layer2'))
+    model.add(Dense(50, input_shape=(784,), activation='relu', name='layer1'))
+    model.add(Dense(50, activation='relu', name='layer2'))
     model.add(Dense(10, activation='softmax', name='layer3'))
     model.summary()
-    rmsprop = optimizers.RMSprop(lr=0.01)
-    model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
+    rmsprop = optimizers.RMSprop(lr)
+    sgd = optimizers.sgd(lr=lr, momentum=0.9, nesterov=True)
+
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    return model
     
 
 def full_connect_5():
@@ -24,6 +27,7 @@ def full_connect_5():
     model.summary()
     rmsprop = optimizers.RMSprop(lr=0.01)
     model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
+    return model
 
 
 def full_connect_8():
@@ -38,42 +42,48 @@ def full_connect_8():
     model.summary()
     rmsprop = optimizers.RMSprop(lr=0.01)
     model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
+    return model
 
 
-def cnn():
+def cnn(lr=0.01):
     model = Sequential()
-    model.add(Reshape((28,28), input_shape=(784,)))
-    model.add(Conv2D(6, (3, 3), activation="relu", padding="valid", name="block1_conv1"))
-    model.add(Conv2D(6, (3, 3), activation="relu", padding="valid", name="block1_conv2"))
+    model.add(Reshape((28,28,1), input_shape=(784,)))
+    model.add(Conv2D(16, (3, 3), activation="relu", padding="valid", name="block1_conv1"))
+    model.add(Conv2D(16, (3, 3), activation="relu", padding="valid", name="block1_conv2"))
     model.add(MaxPooling2D((2,2), name='block1_mp'))
-    model.add(Conv2D(16, (3, 3), activation="relu", padding="valid", name="block2_conv1"))
+    model.add(Conv2D(32, (3, 3), activation="relu", padding="valid", name="block2_conv1"))
     model.add(MaxPooling2D((2,2), name='block2_mp'))
-    model.add(Conv2D(16, (3, 3), activation="relu", padding="valid", name="block3_conv1"))
-    model.add(Conv2D(120, (3, 3), activation="relu", padding="valid", name="block3_conv2"))
+    model.add(Conv2D(32, (3, 3), activation="relu", padding="valid", name="block3_conv1"))
+    model.add(Conv2D(64, (3, 3), activation="relu", padding="valid", name="block3_conv2"))
     model.add(Flatten())
-    model.add(Dense(10, activation='softmax', name='fc1'))
+    model.add(Dense(20, activation='relu', name='fc1'))
+    model.add(Dense(10, activation='softmax', name='fc2'))
     model.summary()
-    sgd = optimizers.sgd(lr=0.01, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    sgd = optimizers.sgd(lr=lr, momentum=0.9, nesterov=True)
+    rmsprop = optimizers.RMSprop(lr)
+    model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
+    return model
 
 
-def cnn_gap():
+def cnn_gap(lr=0.01):
     model = Sequential()
-    model.add(Reshape((28,28), input_shape=(784,)))
-    model.add(Conv2D(128, (3, 3), activation="relu", padding="valid", name="block1_conv1"))
-    model.add(Conv2D(256, (3, 3), activation="relu", padding="valid", name="block1_conv2"))
+    model.add(Reshape((28,28,1), input_shape=(784,)))
+    model.add(Conv2D(64, (3, 3), activation="relu", padding="valid", name="block1_conv1"))
+    model.add(Conv2D(128, (3, 3), activation="relu", padding="valid", name="block1_conv2"))
     model.add(MaxPooling2D((2,2), name='block1_mp'))
-    model.add(Conv2D(512, (3, 3), activation="relu", padding="valid", name="block2_conv1"))
+    model.add(Conv2D(256, (3, 3), activation="relu", padding="valid", name="block2_conv1"))
     model.add(Conv2D(10, (3, 3), activation="relu", padding="valid", name="block2_conv2"))
+    model.add(GlobalAveragePooling2D())
     model.add(Activation('softmax'))
     model.summary()
-    sgd = optimizers.sgd(lr=0.01, momentum=0.9, nesterov=True)
+    sgd = optimizers.sgd(lr=lr, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    return model
 
 
 if __name__ == "__main__":
-    fc_net = full_connect()
-    fc_net5 = full_connect_5()
-    fc_net8 = full_connect_8()
-    cnn1 = cnn()
+    # fc_net = full_connect()
+    # fc_net5 = full_connect_5()
+    # fc_net8 = full_connect_8()
+    # cnn1 = cnn()
     cnn2 = cnn_gap()
--- train.py
+++ train.py
@@ -2,29 +2,37 @@ import tensorflow as tf
 import numpy as np
 import keras
 from data_generator import load_mnist
-from model import full_connect, cnn
+from model import full_connect, cnn, cnn_gap
 
 
 def train():
     # set hyperparameters
-    batchsize = 64
-    epochs = 20
-    lr = 0.01
+    batchsize = 200
+    epochs = 10
+    lr = 0.1
     # get training and testing data
     train_x, train_y = load_mnist('./data', 'train')
     test_x, test_y = load_mnist('./data', 'test')
     # construct model
-    model = full_connect()
-    model.summary()
+    model = cnn_gap(lr)
     # training
-    History = model.fit(train_x, train_y, batch_size=batchsize, epochs=epochs, validation_data=(train_x, train_y),
+    History = model.fit(train_x, train_y, batch_size=batchsize, epochs=epochs, validation_data=(test_x, test_y),
                         shuffle=True)
     print("History-Train:", History, History.history)
-    metrics = model.evaluate(test_x, test_y)
-    print("metrics:", metrics)
+    # metrics = model.evaluate(test_x, test_y)
+    # print("metrics:", metrics)
+    accuracy = eval_accuracy(model.predict(test_x), test_y)
+    print("accuracy:", accuracy)
     # 保存模型
     save_path = './models/model1.h5'
     model.save(save_path)
+    
+
+def eval_accuracy(pred_y, ground_truth_y):
+    pred_y = pred_y.argmax(axis=1).astype('int16')
+    ground_truth_y = ground_truth_y.argmax(axis=1).astype('int16')
+    accuracy = np.mean(np.where(pred_y==ground_truth_y, np.ones_like(pred_y), np.zeros_like(pred_y)))
+    return accuracy
 
 
 if __name__ == "__main__":
